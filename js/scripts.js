f7270c2671591858cb89a37a6fab1e5f90c65240
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-11 19:45:33 -0800

    add cat images, finish js logic for quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
   $("form#cat-quiz").submit(function(event) {
     var name = $("#name").val();
-    //$(".result").append("<p>Great job, "+name+". You got...</p>");
-    $("#spaceship").show();
+    var snuggleInput = $("#snuggle").val();
+    var energyInput = $("input:radio[name=energy]:checked").val();
+    var foodInput = $("#food").val();
+    $(".result").hide();
+    $(".thanks").remove();
+    $(".results").prepend("<p class='thanks'>Thanks, "+name+"! You got...")
+    if (snuggleInput === "Yes" && energyInput !== "stoked" && foodInput === "Tuna") {
+      $("#spaceship").show();
+    } else if (snuggleInput !== "No" && energyInput !== "stoked" && (foodInput === "Potatoes" || foodInput === "Cheese" || foodInput === "Tuna")) {
+      $("#agamemnon").show();
+    } else if (snuggleInput === "Yes" && energyInput !== "stoked" && foodInput === "Peanut Donuts") {
+      $("#gwen").show();
+    } else if (snuggleInput !== "No" && energyInput === "stoked") {
+      $("#tom-petty").show();
+    } else {
+      $("#none").show();
+    }
 
+      //gwen answers
+      //tom petty answers
     event.preventDefault();
   });
 
